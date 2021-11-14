08fe70d253cb175294036e4efd62bcd8a9f40ad8
韩昊天 <haotianhan@126.com>
2021-11-15 02:13:58 +0800

    feat: 重构核心逻辑 & 更新readme

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const xlsx = require("node-xlsx");
-const option = require("./option.json");
 const fs = require("fs");
 const path = require("path");
 const figlet = require("figlet");
 const chalk = require("chalk");
 const ora = require("ora");
 
-const { errHanlders } = require("./lib/utils");
 const {
   OPTION_KEYS,
   OUTPUT_TYPE_OPTIONS,
@@ -14,216 +12,313 @@ const {
   TOOL_PREFIEX,
 } = require("./lib/constants");
 
-let spinner = void 0;
-
-const generateFiles = (results) => {
-  spinner.text = "正在写入文件...";
-
-  const { output } = option;
-  let outputType = OUTPUT_TYPE_OPTIONS.SHEETS;
-
-  if (
-    Object.keys(output).includes(OPTION_KEYS.OUTPUT_TYPE) &&
-    OUTPUT_TYPE_OPTIONS.includes(output.outputType)
-  ) {
-    outputType = output.outputType;
+class Spliter {
+  constructor() {
+    this.option = {};
+    this.spinner = void 0;
   }
 
-  if (outputType === OPTION_KEYS.SHEETS) {
-    const xlsxBuildParams = [];
-    for (const result of results) {
-      xlsxBuildParams.push(getSheetData(result));
+  run(option) {
+    try {
+      this.initSpinner();
+
+      this.checkIsOptionsValid(option);
+
+      this.setOption(option);
+
+      const sheetList = this.parseXlsx();
+
+      const originSheetData = this.getOriginSheetData(sheetList);
+
+      const dataFormatted = this.formatData(originSheetData);
+
+      const resultsFilted = this.filterData(dataFormatted);
+
+      this.generateFiles(resultsFilted);
+    } catch (error) {
+      console.log(error);
     }
-    writeFile("excel拆解结果", xlsxBuildParams);
-  } else {
-    for (const { fileName, formData, extraData } of results) {
-      writeFile(fileName, [getSheetData({ fileName, formData, extraData })]);
+  }
+
+  checkIsOptionsValid(option) {
+    this.spinner.text = "配置格式校验中...";
+    const msgSpinnerFail = "配置格式校验失败";
+
+    if (!option) {
+      return this.errHanlders(msgSpinnerFail, "缺少配置信息");
+    }
+
+    if (Object.prototype.toString.call(option) !== "[object Object]") {
+      return this.errHanlders(msgSpinnerFail, "配置信息格式有误");
+    }
+
+    const { input, output } = option;
+    if (!input) {
+      return this.errHanlders(msgSpinnerFail, `缺少[${OPTION_KEYS.INPUT}]配置`);
+    }
+
+    if (!output) {
+      return this.errHanlders(
+        msgSpinnerFail,
+        `缺少[${OPTION_KEYS.OUTPUT}]配置`
+      );
+    }
+
+    if (!Object.keys(input).includes(OPTION_KEYS.PATH)) {
+      return this.errHanlders(
+        msgSpinnerFail,
+        `缺少[${OPTION_KEYS.INPUT}.${OPTION_KEYS.PATH}]配置`
+      );
+    }
+
+    if (!Object.keys(output).includes(OPTION_KEYS.PATH)) {
+      return this.errHanlders(
+        msgSpinnerFail,
+        `缺少[${OPTION_KEYS.OUTPUT}.${OPTION_KEYS.PATH}]配置`
+      );
+    }
+
+    if (!Object.keys(output).includes(OPTION_KEYS.ACTIONS)) {
+      return this.errHanlders(
+        msgSpinnerFail,
+        `缺少[${OPTION_KEYS.OUTPUT}.${OPTION_KEYS.ACTIONS}]配置`
+      );
+    }
+
+    return this.spinner.succeed("配置格式校验成功");
+  }
+
+  setOption(option) {
+    this.option = option;
+  }
+
+  initSpinner() {
+    this.spinner = ora("脚本启动");
+    this.spinner.color = "green";
+    this.spinner.start();
+  }
+
+  spinnerError(msg) {
+    this.spinner.stop();
+    this.spinner.fail(msg);
+  }
+
+  errHanlders(msgSpinner, msgChalk) {
+    this.spinnerError(msgSpinner);
+    console.log(`${chalk.red(`${TOOL_PREFIEX}${msgChalk}`)} `);
+    throw Error(msgChalk);
+  }
+
+  parseXlsx() {
+    this.spinner.text = "Excel读取中...";
+
+    const {
+      input: { path },
+    } = this.option;
+
+    let sheetList = [];
+    try {
+      sheetList = xlsx.parse(path);
+    } catch (error) {
+      return this.errHanlders(
+        "Excel读取失败",
+        "解析excel文件失败，请检查文件路径以及文件名后缀"
+      );
+    }
+    this.spinner.succeed("Excel读取完毕");
+    return sheetList;
+  }
+
+  getOriginSheetData(sheetList) {
+    this.spinner.text = "获取原始Excel的Sheet数据...";
+
+    let sheet = "";
+    if (Object.keys(this.option.input).includes(OPTION_KEYS.SHEET)) {
+      sheet = this.option.input.sheet;
+    } else {
+      sheet = "Sheet1";
+    }
+    const index = sheetList.findIndex(({ name }) => sheet === name);
+    if (index === -1) {
+      return this.errHanlders(
+        "获取Sheet数据失败",
+        "没有在Excel中找到对应的sheet，请检查[option.file.sheet]是否合法"
+      );
+    }
+
+    this.spinner.succeed("获取Sheet数据成功");
+    return sheetList[index].data || [];
+  }
+
+  formatData(sheetData) {
+    this.spinner.text = "格式化Sheet数据...";
+    const errMsg = "格式化Sheet数据失败";
+
+    const dataFormatted = {
+      rows: [],
+      data: [],
+    };
+
+    try {
+      sheetData.forEach((row, index) => {
+        if (index === 0) {
+          dataFormatted.rows = row;
+        } else {
+          const obj = {};
+          dataFormatted.rows.forEach((key, index) => {
+            obj[key] = row[index];
+          });
+          dataFormatted.data.push(obj);
+        }
+      });
+    } catch (error) {
+      return this.errHanlders(errMsg, errMsg);
+    }
+
+    this.spinner.succeed("格式化Sheet数据成功");
+    return dataFormatted;
+  }
+
+  filterData(dataFromatted) {
+    this.spinner.text = "根据查询条件拆解表格中...";
+
+    const {
+      output: { actions = {} },
+    } = this.option;
+
+    const results = [];
+    for (const actionType of Object.keys(actions)) {
+      results.push(
+        this.generateChildExcel(actionType, actions[actionType], dataFromatted)
+      );
+    }
+
+    this.spinner.succeed("表格数据拆解成功");
+    return results;
+  }
+
+  generateChildExcel(key, action, originFormData) {
+    const msgSpinnerFail = "表格数据拆解失败";
+    let formData = JSON.parse(JSON.stringify(originFormData));
+    let formList = formData.data || [];
+    if (!Object.keys(action).includes(ACTION_LIST.search)) {
+      return this.errHanlders(
+        msgSpinnerFail,
+        `配置缺少[${OPTION_KEYS.OUTPUT}.${OPTION_KEYS.ACTIONS}.[fileName].${OPTION_KEYS.SEARCH}]字段`
+      );
+    }
+
+    // 根据查询配置过滤数据
+    const searchActions = action[ACTION_LIST.search];
+    if (!Array.isArray(searchActions)) {
+      return this.errHanlders(msgSpinnerFail, "[search]配置必须是Array类型】");
+    }
+    searchActions.forEach(({ col, value }) => {
+      formList = formList.filter((item) => item[col] === value);
+    });
+    formData.data = formList;
+
+    const extraData = [];
+
+    // 计算数据条数
+    if (Object.keys(action).includes(ACTION_LIST.count)) {
+      const countAction = action[ACTION_LIST.count];
+      extraData.push({ col: countAction.col, data: formList.length });
+    }
+
+    // 计算某些列的值的和
+    if (Object.keys(action).includes(ACTION_LIST.sum)) {
+      const sumActions = action[ACTION_LIST.sum];
+      sumActions.forEach(({ col }) => {
+        let total = 0;
+        formData.data.forEach((item) => {
+          let value = 0;
+          if (item[col] && typeof item[col] === "number") {
+            value = item[col];
+          }
+          total += value;
+        });
+        extraData.push({ col, data: total });
+      });
+    }
+    return { fileName: key, formData, extraData };
+  }
+
+  generateFiles(results) {
+    this.spinner.text = "正在生成excel文件...";
+
+    const { output } = this.option;
+    let outputType = OUTPUT_TYPE_OPTIONS.SHEETS;
+
+    if (
+      Object.keys(output).includes(OPTION_KEYS.OUTPUT_TYPE) &&
+      OUTPUT_TYPE_OPTIONS.includes(output.outputType)
+    ) {
+      outputType = output.outputType;
+    }
+
+    if (outputType === OPTION_KEYS.SHEETS) {
+      const xlsxBuildParams = [];
+      for (const result of results) {
+        xlsxBuildParams.push(this.getSheetDataFormatted(result));
+      }
+      this.writeFile("result", xlsxBuildParams);
+    } else {
+      for (const { fileName, formData, extraData } of results) {
+        this.writeFile(fileName, [
+          this.getSheetDataFormatted({ fileName, formData, extraData }),
+        ]);
+      }
     }
   }
-};
 
-const writeFile = (fileName, xlsxBuildParams) => {
-  try {
-    fs.writeFileSync(
-      path.resolve(__dirname, `./${OPTION_KEYS.OUTPUT_DIR}/${fileName}.xlsx`),
-      xlsx.build(xlsxBuildParams)
-    );
-    spinner.stop();
-    spinner.succeed("写入文件成功");
-    console.log(`${chalk.green(`${TOOL_PREFIEX}生成${fileName}.xlsx成功`)} `);
-    console.log(
-      "\r\n" +
-        figlet.textSync("excel-spliter", {
-          horizontalLayout: "default",
-          verticalLayout: "default",
-          width: 100,
-          whitespaceBreak: true,
-        })
-    );
-  } catch (error) {
-    return console.log(err);
-  }
-};
+  getSheetDataFormatted({ fileName: name, formData, extraData }) {
+    const sheetData = {
+      name,
+      data: [formData.rows],
+    };
+    formData.data.forEach((item) => {
+      let arr = new Array(formData.rows.length).fill(void 0);
+      formData.rows.forEach((col, colIndex) => {
+        arr[colIndex] = item[col];
+      });
+      sheetData.data.push(arr);
+    });
 
-const getSheetData = ({ fileName: name, formData, extraData }) => {
-  const sheetData = {
-    name,
-    data: [formData.rows],
-  };
-  formData.data.forEach((item) => {
     let arr = new Array(formData.rows.length).fill(void 0);
-    formData.rows.forEach((col, colIndex) => {
-      arr[colIndex] = item[col];
+    extraData.forEach(({ col, data }) => {
+      const index = formData.rows.findIndex((rowName) => rowName === col);
+      if (index > -1) {
+        arr[index] = data;
+      }
     });
     sheetData.data.push(arr);
-  });
+    return sheetData;
+  }
 
-  let arr = new Array(formData.rows.length).fill(void 0);
-  extraData.forEach(({ col, data }) => {
-    const index = formData.rows.findIndex((rowName) => rowName === col);
-    if (index > -1) {
-      arr[index] = data;
+  writeFile(fileName, xlsxBuildParams) {
+    try {
+      fs.writeFileSync(
+        path.resolve(this.option.output.path, `./${fileName}.xlsx`),
+        xlsx.build(xlsxBuildParams)
+      );
+
+      this.spinner.stop();
+      this.spinner.succeed("写入文件成功");
+      console.log(`${chalk.green(`${TOOL_PREFIEX}生成${fileName}.xlsx成功`)} `);
+      console.log(
+        "\r\n" +
+          figlet.textSync("excel-spliter", {
+            horizontalLayout: "default",
+            verticalLayout: "default",
+            width: 100,
+            whitespaceBreak: true,
+          })
+      );
+    } catch (error) {
+      return console.log(error);
     }
-  });
-  sheetData.data.push(arr);
-  return sheetData;
-};
-
-const generateChildExcel = (key, action, originFormData) => {
-  let formData = JSON.parse(JSON.stringify(originFormData));
-  let formList = formData.data || [];
-  if (!Object.keys(action).includes(ACTION_LIST.search)) {
-    errHanlders("请检查options.json里某个表缺少【search】字段，请检查！");
   }
+}
 
-  // 根据查询配置过滤数据
-  const searchActions = action[ACTION_LIST.search];
-  if (!Array.isArray(searchActions)) {
-    errHanlders("【search】字段必须是【数组类型】，请检查！");
-  }
-  searchActions.forEach(({ col, value }) => {
-    formList = formList.filter((item) => item[col] === value);
-  });
-  formData.data = formList;
-
-  const extraData = [];
-
-  // 计算数据条数
-  if (Object.keys(action).includes(ACTION_LIST.count)) {
-    const countAction = action[ACTION_LIST.count];
-    extraData.push({ col: countAction.col, data: formList.length });
-  }
-
-  // 计算某些列的值的和
-  if (Object.keys(action).includes(ACTION_LIST.sum)) {
-    const sumActions = action[ACTION_LIST.sum];
-    sumActions.forEach(({ col }) => {
-      let total = 0;
-      formData.data.forEach((item) => {
-        let value = 0;
-        if (item[col] && typeof item[col] === "number") {
-          value = item[col];
-        }
-        total += value;
-      });
-      extraData.push({ col, data: total });
-    });
-  }
-  return { fileName: key, formData, extraData };
-};
-
-const parseXlsx = async (option) => {
-  spinner = ora("Excel读取中...");
-  spinner.color = "green";
-  spinner.start();
-
-  checkIsOptionsValid(option);
-  const {
-    input: { name },
-    output,
-  } = option;
-
-  let sheetList = null;
-  try {
-    sheetList = xlsx.parse(`./${OPTION_KEYS.INPUT_DIR}/${name}.xlsx`);
-  } catch (error) {
-    return console.log(
-      `${chalk.red(
-        `${TOOL_PREFIEX}${"解析excel文件失败，请检查文件路径以及文件名后缀"}`
-      )} `
-    );
-  }
-
-  spinner.succeed("Excel读取完毕");
-  spinner.text = "Excel拆解中...";
-
-  // 默认把拆表结果放到一个excel的多个sheet中
-  const { actions = {} } = output;
-  const formData = formatData(getOriginExcelData(sheetList));
-
-  const results = [];
-  spinner.succeed("Excel拆解完毕");
-  spinner.text = "正在生成子表...";
-  for (const actionType of Object.keys(actions)) {
-    const formDataFilted = generateChildExcel(
-      actionType,
-      actions[actionType],
-      formData
-    );
-    results.push(formDataFilted);
-  }
-
-  generateFiles(results);
-};
-
-const checkIsOptionsValid = (option) => {
-  const { input, output } = option;
-  if (!input) {
-    errHanlders(`[option]缺少[${OPTION_KEYS.INPUT}]配置`);
-  }
-
-  if (!output) {
-    errHanlders(`[option]缺少[${OPTION_KEYS.OUTPUT}]配置`);
-  }
-
-  if (!Object.keys(input).includes(OPTION_KEYS.NAME)) {
-    errHanlders(`[option.input]缺少[${OPTION_KEYS.NAME}]配置`);
-  }
-};
-
-const formatData = (sheetData) => {
-  const formData = {
-    rows: [],
-    data: [],
-  };
-  sheetData.forEach((row, index) => {
-    if (index === 0) {
-      formData.rows = row;
-    } else {
-      const obj = {};
-      formData.rows.forEach((key, index) => {
-        obj[key] = row[index];
-      });
-      formData.data.push(obj);
-    }
-  });
-  return formData;
-};
-
-const getOriginExcelData = (sheetList) => {
-  let sheet = "";
-  if (Object.keys(option.input).includes(OPTION_KEYS.SHEET)) {
-    sheet = option.input.sheet;
-  } else {
-    sheet = "Sheet1";
-  }
-  const index = sheetList.findIndex(({ name }) => sheet === name);
-  if (index === -1) {
-    errHanlders(
-      "没有在Excel中找到对应的sheet，请检查[option.file.sheet]是否合法"
-    );
-  }
-  return sheetList[index].data || [];
-};
-
-parseXlsx(option);
+module.exports = new Spliter();
--- readme.md
+++ readme.md
@@ -12,4 +12,128 @@ $ npm install node-excel-spliter
 
 ## Usage 使用方法
 
-待完善
+```js
+const spliter = require("node-excel-spliter");
+spliter.run({
+  input: { path: "/Users/hanhaotian/Desktop/测试.xlsx" },
+  output: {
+    path: "/Users/hanhaotian/Desktop/output",
+    outputType: "sheets",
+    actions: {
+      子表1: {
+        search: [
+          {
+            col: "类型",
+            value: "器件类",
+          },
+          {
+            col: "名称",
+            value: "器件类AAA",
+          },
+        ],
+        count: {
+          col: "合计",
+        },
+        sum: [
+          {
+            col: "1轮",
+          },
+          {
+            col: "2轮",
+          },
+          {
+            col: "3轮",
+          },
+          {
+            col: "4轮",
+          },
+        ],
+      },
+      子表2: {
+        search: [
+          {
+            col: "类型",
+            value: "产成品",
+          },
+          {
+            col: "名称",
+            value: "产成品AAA",
+          },
+        ],
+        count: {
+          col: "合计",
+        },
+        sum: [
+          {
+            col: "1轮",
+          },
+          {
+            col: "2轮",
+          },
+          {
+            col: "3轮",
+          },
+          {
+            col: "4轮",
+          },
+        ],
+      },
+    },
+  },
+});
+```
+
+## Configs Detail 配置参数说明
+
+### input
+
+Type: `object`
+
+原始 Excel 文件相关的配置
+
+### input.path
+
+Type: `string`
+
+原始 Excel 文件在磁盘中的绝对路径
+
+### output
+
+Type: `object`
+
+输出的 Excel 文件相关的配置
+
+### output.path
+
+Type: `string`
+
+输出的 Excel 目标文件夹在磁盘中的绝对路径
+
+### output.path.outputType?
+
+Type:`sheets|files`\
+Default: `sheets`
+
+值为 sheets 则在一个 Excel 文件中生成多个 sheet；值为 files 生成多个 Excel 文件
+
+### output.path.actions
+
+Type: `object`
+
+对原始 Excel 的拆解规则
+
+### output.path.actions.[sheet]
+
+Type: `object`
+
+在需要拆解生成的 sheets 下定义拆解规则
+
+### output.path.actions.[sheet].[action]
+
+Type: `search|count|sum`
+
+`1.例如配置search.col=="类型"，search.value=="器件类"，则会根据“找到原始Excel中所有【类型】列的值为【器件类】的数据”，可配置多个搜索条件的并集`
+
+`2.例如配置sum.col=="1轮"，则在完成表格拆解后，会自动计算所有数据【1轮】列的值的和，可配置多个列自动求和`
+
+`3.例如配置count.col=="合计"，则在完成表格拆解后，会自动统计所有数据的条数，并写入在最后一条数据的【合计】列的下面`
